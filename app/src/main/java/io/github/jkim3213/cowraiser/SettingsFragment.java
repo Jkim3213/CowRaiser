c85f3da3f07de7fa6021f2798dd2c92e2969315a
Jin Woo Lee <tpfkvla1234@gmail.com>
2019-02-12 17:01:20 -0500

    Revert "Merge branch 'master' of https://github.com/Jkim3213/CowRaiser"
    This reverts commit 8706fcb5dcea0f1f0248ee675681dc1612a79b11, reversing
    changes made to 60f66b7d4682b36a19bf55ae6a506d464a53b48e.

--- app/src/main/java/io/github/jkim3213/cowraiser/SettingsFragment.java
+++ app/src/main/java/io/github/jkim3213/cowraiser/SettingsFragment.java
@@ -1,76 +1,18 @@
 package io.github.jkim3213.cowraiser;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import static android.support.constraint.Constraints.TAG;
-//logout functionality working now
 public class SettingsFragment extends Fragment {
 
-    //private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private Button msignOut;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View view  = inflater.inflate(R.layout.fragment_settings, container, false);
-        msignOut = (Button) view.findViewById(R.id.sign_out);
-
-        setupFirebaseListner();
-
-        msignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onclick: signing out the user");
-                FirebaseAuth.getInstance().signOut();
-            }
-        });
-
-        return view;
-    }
-    private void setupFirebaseListner() {
-
-        Log.d(TAG, "setupFirebaseListner: setting up the auth state listener");
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user =  firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
-                } else {
-
-                    Log.d(TAG, "onAuthStateChanged: signed out");
-                    Toast.makeText(getActivity(),"signed out", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getActivity(), LoginActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                }
-            }
-        };
-    }
-    @Override
-    public void onStart(){
-        super.onStart();
-        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
-    }
-    @Override
-    public void onStop(){
-        super.onStop();
-        if(mAuthStateListener != null) {
-            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
-        }
+        return inflater.inflate(R.layout.fragment_settings, null);
     }
 }
