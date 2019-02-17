4533f73172f3f4c6a4e64a237f4a05e3ad1bf294
cford35 <cford35@gatech.edu>
2019-02-17 16:48:06 -0500

    drag n drop

--- app/src/main/java/io/github/jkim3213/cowraiser/HomeFragment.java
+++ app/src/main/java/io/github/jkim3213/cowraiser/HomeFragment.java
@@ -1,16 +1,20 @@
 package io.github.jkim3213.cowraiser;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +25,15 @@ public class HomeFragment extends Fragment {
 
     private ViewGroup haven;
     private View view;
+
+    //moveable object vars
+    private ViewGroup mainLayout;
+    private ImageView image1;
+    private ImageView image2;
+
+    private int xDelta;
+    private int yDelta;
+    //
 
     @Nullable
     @Override
@@ -47,7 +60,12 @@ public class HomeFragment extends Fragment {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
         myListView.setAdapter(arrayAdapter);
         haven = view.findViewById(R.id.haven);
+        mainLayout = (RelativeLayout) view.findViewById(R.id.main);
+        image1 = (ImageView) view.findViewById(R.id.image1);
+        image2 = (ImageView) view.findViewById(R.id.image2);
 
+        image1.setOnTouchListener(onTouchListener());
+        image2.setOnTouchListener(onTouchListener());
 
 
         Toast.makeText(getContext(), "Welcome to Home.", Toast.LENGTH_SHORT).show();
@@ -108,7 +126,45 @@ public class HomeFragment extends Fragment {
 
         }
 
-
     }
 
+    //--------------------------------------------------------
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                .getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        };
+    }
 }
+
+
+
